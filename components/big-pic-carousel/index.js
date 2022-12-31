8a98b0fadd3b0145d6f4ea98863845aa3fcdadf6
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-12-31 01:09:04 -0300

    Username addition and slide move

--- components/big-pic-carousel/index.js
+++ components/big-pic-carousel/index.js
@@ -6,24 +6,20 @@ import { useState } from 'react';
 
 const BigPicCarousel = () => {
   const [list, setList] = useState([
-    { description: 'Audi RS5 2020' },
-    { description: 'Audi RS5 2021' },
-    { description: 'Audi RS5 2022' },
-    { description: 'Audi RS5 2023' },
+    { description: 'Audi RS5 2020', userName: 'Shmee150' },
+    { description: 'Audi RS5 2021', userName: 'Toneko' },
+    { description: 'Audi RS5 2022', userName: 'Juancito' },
+    { description: 'Audi RS5 2023', userName: 'Pepito' },
   ]);
   return (
     <div className={styles.container}>
       {list.map((car) => {
         return (
           <span className={styles.itemContainer} key={car.description}>
-            <Image
-              // priority
-              // src="/images/cars/car1.jpg"
-              className={styles.pic}
-              // la
-              src={carImage}
-              alt="car image"
-            />
+            <div className={styles.imageContainer}>
+              <Image className={styles.pic} src={carImage} alt="car image" />
+              <div className={styles.userName}>@{car.userName}</div>
+            </div>
             <div className={styles.backPannel}>{car.description}</div>
           </span>
         );
