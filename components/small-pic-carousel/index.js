a706dce9ed6d218f1594af3297292b77570b9fde
Damelanote2501 <nicolastonelli1406@gmail.com>
2022-12-31 21:11:28 -0300

    Cleanup

--- components/small-pic-carousel/index.js
+++ components/small-pic-carousel/index.js
@@ -10,9 +10,6 @@ const SmallPicCarousel = () => {
     { description: 'Audi RS5 2021', userName: 'Toneko' },
     { description: 'Audi RS5 2022', userName: 'Juancito' },
     { description: 'Audi RS5 2023', userName: 'Pepito' },
-    { description: 'Audi RS5 2023', userName: 'Pepito' },
-    { description: 'Audi RS5 2023', userName: 'Pepito' },
-    { description: 'Audi RS5 2023', userName: 'Pepito' },
   ]);
   return (
     <div className={styles.container}>
