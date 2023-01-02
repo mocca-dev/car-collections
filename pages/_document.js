c57ef5c36c8dfcafea6db45e04fb2bd9bb1d5da5
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-01-01 22:17:27 -0300

    PWAfication

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/512x512.png"></link>
+          <meta name="theme-color" content="#2a2438" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
