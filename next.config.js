a474bfe8dd69c822566508a5495fea64fe0d9079
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-01-04 19:24:26 -0300

    GenerateSW PWA terminal warn fix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 });
 
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: prod ? false : true,
   },
 });
