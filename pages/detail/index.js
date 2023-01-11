d6d77ff966c30259ecadfdc4629d1982ed279a3a
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-01-10 21:24:27 -0300

    Detail description scroll addition

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -57,14 +57,53 @@ const Detail = () => {
           </Link>
         </div>
         <h3>Description</h3>
-        <p className={styles.description}>
-          Gracias al diseño progresivo con un aspecto frontal definido con
-          precisión y una trasera característica, el carácter deportivo del Audi
-          A4 Sedan se manifiesta más que nunca: un interior premium y funcional,
-          tecnologías innovadoras en los ámbitos de la digitalización, el
-          infoentretenimiento y los sistemas de asistencia al conductor, y una
-          gama de potentes motores completan el paquete.
-        </p>
+        <div className={styles.descriptionContainer}>
+          <div className={styles.fadeOut}></div>
+          <p className={styles.description}>
+            Gracias al diseño progresivo con un aspecto frontal definido con
+            precisión y una trasera característica, el carácter deportivo del
+            Audi A4 Sedan se manifiesta más que nunca: un interior premium y
+            funcional, tecnologías innovadoras en los ámbitos de la
+            digitalización, el infoentretenimiento y los sistemas de asistencia
+            al conductor, y una gama de potentes motores completan el paquete.
+            Gracias al diseño progresivo con un aspecto frontal definido con
+            precisión y una trasera característica, el carácter deportivo del
+            Audi A4 Sedan se manifiesta más que nunca: un interior premium y
+            funcional, tecnologías innovadoras en los ámbitos de la
+            digitalización, el infoentretenimiento y los sistemas de asistencia
+            al conductor, y una gama de potentes motores completan el paquete.
+            gama de potentes motores completan el paquete. Gracias al diseño
+            progresivo con un aspecto frontal definido con precisión y una
+            trasera característica, el carácter deportivo del Audi A4 Sedan se
+            manifiesta más que nunca: un interior premium y funcional,
+            tecnologías innovadoras en los ámbitos de la digitalización, el
+            infoentretenimiento y los sistemas de asistencia al conductor, y una
+            gama de potentes motores completan el paquete. gama de potentes
+            motores completan el paquete. Gracias al diseño progresivo con un
+            aspecto frontal definido con precisión y una trasera característica,
+            el carácter deportivo del Audi A4 Sedan se manifiesta más que nunca:
+            un interior premium y funcional, tecnologías innovadoras en los
+            ámbitos de la digitalización, el infoentretenimiento y los sistemas
+            de asistencia al conductor, y una gama de potentes motores completan
+            el paquete. motores completan el paquete. Gracias al diseño
+            progresivo con un aspecto frontal definido con precisión y una
+            trasera característica, el carácter deportivo del Audi A4 Sedan se
+            manifiesta más que nunca: un interior premium y funcional,
+            tecnologías innovadoras en los ámbitos de la digitalización, el
+            infoentretenimiento y los sistemas de asistencia al conductor, y una
+            gama de potentes motores completan el paquete. gama de potentes
+            motores completan el paquete. Gracias al diseño progresivo con un
+            aspecto frontal definido con precisión y una trasera característica,
+            el carácter deportivo del Audi A4 Sedan se manifiesta más que nunca:
+            un interior premium y funcional, tecnologías innovadoras en
+            AAAAAAAAAAAAos ámbitos de la digitalización, el infoentretenimiento
+            y los sistemas de asistencia al conductor, y una gama de potentes
+            motores completan el paquete.
+            <br />
+            <br />
+            <br />
+          </p>
+        </div>
       </div>
       <div className={styles.pic}></div>
     </Layout>
