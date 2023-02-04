49e6c7272fa931437d7479b8869c7fe9364c3e0e
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-02-04 20:09:03 -0300

    Litle update to next.condig

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 });
 
-const prod = process.env.NODE_ENV === 'production';
-
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: prod ? false : true,
+    disable: process.env.NODE_ENV === 'development',
   },
 });
